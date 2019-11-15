de00c154a20ba3966dd35a77c3a94c3d1284cb88
Glitch (fcc-microservice-5-aerlaut) <none>
2019-11-15 02:56:39 +0000

    Initial commit

--- server.js
+++ server.js
@@ -2,22 +2,24 @@
 
 var express = require('express');
 var cors = require('cors');
+var fileUpload = require('express-fileupload');
 
 // require and use "multer"...
 
 var app = express();
 
 app.use(cors());
+app.use(fileUpload());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
 });
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
+
+app.post('/api/fileanalyse', function(req, res) {
+  res.json({'name' : req.files.upfile.name, 'size': req.files.upfile.size })
+})
